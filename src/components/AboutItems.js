6b5cb6343b5fada97fe9428f967dd4c90e906370
Greg <greg_markarian@hotmail.com>
2021-10-08 16:40:41 +0300

    ADD: Components mapping process

---
+++ src/components/AboutItems.js
@@ -0,0 +1,22 @@
+import React from "react";
+import cn from "classnames";
+
+function AboutItems(props) {
+  return (
+    <div>
+      <div className={cn("flex")}>
+        <div className={cn("flex-shrink")}>
+          <img className = {cn("rounded-full w-60 h-24")} src={props.imageUrl} alt="" />
+        </div>
+        <div className={cn("flex-shrink pl-3")}>
+         <p className = {cn("italic 2xl")}>{props.title}</p>
+         <p className= {cn("text-sm text-gray-500")}>{props.description}</p>
+         <p className={cn("text-sm text-red-500")}>{props.readMore}</p>
+        </div>
+       
+      </div>
+    </div>
+  );
+}
+
+export default AboutItems;
