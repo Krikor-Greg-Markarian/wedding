21154ffeeea8b7dae6597174fff0e4993346a3fd
Greg <greg_markarian@hotmail.com>
2021-10-08 17:50:45 +0300

    ADD: bridegroom item pictures text description components

---
+++ src/components/BrideGroomItem.js
@@ -0,0 +1,24 @@
+import React from "react";
+import cn from "classnames";
+
+function BrideGroomItem(props) {
+  return (
+    <div className={cn("")}>
+      <div className={cn("flex justify-center items-center")}>
+        <img
+          className={cn("rounded-full w-40 h-40")}
+          src={props.imageUrl}
+          alt="bridesmaid image"
+        />
+      </div>
+      <div className={cn("flex justify-center items-center pt-3")}>
+        <p className={cn("font-bold text-sm")}>{props.name}</p>
+      </div>
+      <div className={cn("flex justify-center items-center")}>
+        <p className={cn("text-red-500 text-sm")}>{props.description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default BrideGroomItem;
