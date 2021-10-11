cdfa307f588bbf13200247b7f41818d02ad61466
Greg <greg_markarian@hotmail.com>
2021-10-11 13:02:01 +0300

    ADD: flex justify-center

--- src/components/AboutItems.js
+++ src/components/AboutItems.js
@@ -4,17 +4,21 @@ import ReadMoreButton from "./ReadMoreButton";
 
 function AboutItems(props) {
   return (
-    <div>
-      <div className={cn("flex")}>
-        <div className={cn("flex-shrink")}>
-          <img className = {cn("rounded-full w-60 h-24")} src={props.imageUrl} alt="" />
-        </div>
-        <div className={cn("flex-shrink pl-3")}>
-         <p className = {cn("italic 2xl")}>{props.title}</p>
-         <p className= {cn("text-sm text-gray-500")}>{props.description}</p>
-         <ReadMoreButton className = {cn("text-red-500 text-sm")} ReadMoreButtonName={"READ MORE"} />
-        </div>
-       
+    <div className={cn("flex pt-14")}>
+      <div className={cn("flex-shrink")}>
+        <img
+          className={cn("rounded-full w-60 h-20 pt-2")}
+          src={props.imageUrl}
+          alt=""
+        />
+      </div>
+      <div className={cn("flex-shrink pl-3")}>
+        <p className={cn("italic 2xl ")}>{props.title}</p>
+        <p className={cn("text-sm text-gray-500 pt-2")}>{props.description}</p>
+        <ReadMoreButton
+          className={cn("text-red-500 text-sm pt-5")}
+          ReadMoreButtonName={"READ MORE"}
+        />
       </div>
     </div>
   );
