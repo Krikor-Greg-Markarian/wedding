f9e8011bcc5d9c452e123b1cf7c5528978f8576f
Greg <greg_markarian@hotmail.com>
2021-10-11 15:23:11 +0300

    ADD: navbar 2

---
+++ src/components/NavbarTwo.js
@@ -0,0 +1,62 @@
+import React from "react";
+import cn from "classnames";
+import Link from "next/link";
+
+function NavbarTwo(props) {
+  return (
+    <div>
+      <div className={cn("oliveleave")}>
+        <div className={cn("flex justify-center items-center p-8")}>
+          <li className={cn("inline text-red-500 text-sm")}>
+            <Link href="/">
+              <a href="">{props.homeNavbar}</a>
+            </Link>
+          </li>
+          <li
+            className={cn("inline text-white pl-10 text-sm hover:text-red-500")}
+          >
+            <Link href="/">
+              <a href="">{props.theWeddingNavbar}</a>
+            </Link>
+          </li>
+          <li
+            className={cn("inline text-white pl-10 text-sm hover:text-red-500")}
+          >
+            <Link href="journal">
+              <a href="">{props.journalNavbar}</a>
+            </Link>
+          </li>
+          <li
+            className={cn(
+              "inline text-white pl-10  text-sm hover:text-red-500"
+            )}
+          >
+            <Link href="/">
+              <a href="">{props.photosNavbar}</a>
+            </Link>
+          </li>
+          <li
+            className={cn(
+              "inline text-white pl-10  text-sm hover:text-red-500"
+            )}
+          >
+            <Link href="/">
+              <a href="">{props.rvspNavbar}</a>
+            </Link>
+          </li>
+          <li
+            className={cn(
+              "inline text-white pl-10  text-sm hover:text-red-500"
+            )}
+          >
+            <Link href="/">
+              <a href="">{props.contactNavbar}</a>
+            </Link>
+          </li>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default NavbarTwo;
