abea6ffc50dc201a3ac1206fceed1d5da97fabaf
Greg <greg_markarian@hotmail.com>
2021-10-11 17:49:05 +0300

    ADD: recent item coponnet

---
+++ src/components/RecentItems.js
@@ -0,0 +1,15 @@
+import React from "react";
+import cn from "classnames";
+
+function RecentItems(props) {
+  return (
+    <div>
+      <p className={cn("border-b-2 p-2 text-sm")}>{props.text1}</p>
+      <p className={cn("border-b-2 p-2 text-sm")}>{props.text2}</p>
+      <p className={cn("border-b-2 p-2 text-sm")}>{props.text3}</p>
+      <p className={cn("border-b-2 p-2 text-sm")}>{props.text4}</p>
+    </div>
+  );
+}
+
+export default RecentItems;
