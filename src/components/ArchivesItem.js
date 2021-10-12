abb98cab6385844ab643dc66cab9ff381423b059
Greg <greg_markarian@hotmail.com>
2021-10-12 09:33:34 +0300

    FIX: w-6/12 div section

--- src/components/ArchivesItem.js
+++ src/components/ArchivesItem.js
@@ -3,7 +3,7 @@ import cn from "classnames";
 
 function ArchivesItem(props) {
   return (
-    <div>
+    <div className={cn("w-6/12")}>
       <p className={cn("border-b-2 p-2")}>{props.date}</p>
       <p className={cn("border-b-2 p-2")}>{props.anotherDate}</p>
     </div>
