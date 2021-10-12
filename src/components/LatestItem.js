7421ea801fc397992d38700deec9bdac27c5c86a
Greg <greg_markarian@hotmail.com>
2021-10-12 09:34:38 +0300

    FIX: resized the image size rouned

--- src/components/LatestItem.js
+++ src/components/LatestItem.js
@@ -4,14 +4,17 @@ import cn from "classnames";
 function LatestItem(props) {
   return (
     <div>
-      <div className={cn("flex border-b-2 pb-3 pt-2")}>
-        <div className={cn("flex-shrink")}>
-          <img className = {cn("rounded-full w-20 h-16")} src={props.imageUrl} alt="" />
+      <div className={cn("flex  ")}>
+        <div className={cn("flex-shrink border-b-2 pb-3 pt-2")}>
+          <img
+            className={cn("rounded-full w-20 h-20 ")}
+            src={props.imageUrl}
+            alt=""
+          />
         </div>
-        <div className={cn("flex-shrink pl-3 text-sm pt-2")}>
+        <div className={cn("flex-shrink pl-3 text-sm pt-2 w-5/12 border-b-2")}>
           <p>{props.text}</p>
         </div>
-       
       </div>
     </div>
   );
