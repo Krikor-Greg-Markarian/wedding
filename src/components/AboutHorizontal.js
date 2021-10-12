eca91be4b9f63aee67d7f7a80dc0b4a9054b66c2
Greg <greg_markarian@hotmail.com>
2021-10-12 09:32:52 +0300

    FIX: fixed width and size of rounded pictures

--- src/components/AboutHorizontal.js
+++ src/components/AboutHorizontal.js
@@ -4,16 +4,20 @@ import cn from "classnames";
 function AboutHorizontal(props) {
   return (
     <div>
-      <div className={cn("flex justify-center items-center")}>
+      <div className={cn("flex justify-center items-center w-6/12")}>
         <img
-          className={cn("rounded-full w-20 h-20")}
+          className={cn("rounded-full w-40 h-40 ")}
           src={props.imageUrlSara}
           alt="girl picture"
         />
       </div>
 
-      <p className={cn("text-sm text-center pt-2 pb-2")}>{props.girlName}</p>
-      <p className={cn("text-sm text-gray-500 text-center")}>{props.text}</p>
+      <p className={cn("text-sm text-center pt-3 pb-2 w-6/12 ")}>
+        {props.girlName}
+      </p>
+      <p className={cn("text-sm text-gray-500 text-center w-6/12")}>
+        {props.text}
+      </p>
     </div>
   );
 }
