dac598100236c59c4a5099e8011e2c78f01a2da8
Greg <greg_markarian@hotmail.com>
2021-10-12 09:34:53 +0300

    ADD: w-6/12 to the div section

--- src/components/RecentItems.js
+++ src/components/RecentItems.js
@@ -3,7 +3,7 @@ import cn from "classnames";
 
 function RecentItems(props) {
   return (
-    <div>
+    <div className={cn("w-6/12")}>
       <p className={cn("border-b-2 p-2 text-sm")}>{props.text1}</p>
       <p className={cn("border-b-2 p-2 text-sm")}>{props.text2}</p>
       <p className={cn("border-b-2 p-2 text-sm")}>{props.text3}</p>
