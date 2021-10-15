04f675565b6c570a529ec48e9b95ac476d034293
Greg <greg_markarian@hotmail.com>
2021-10-15 16:43:10 +0300

    ADD: latest journal stories

--- pages/api/thewedding.js
+++ pages/api/thewedding.js
@@ -36,6 +36,39 @@ export default function handler(req, res) {
         ReadMoreButtonName: "READ MORE",
       },
     ],
+    latestjournalstories: [
+      {
+        imageURl:
+          "http://themes.sindevo.com/marriage-full/wp-content/uploads/2017/11/thumb1.jpg",
+        text: "Let us always meet each other with smile, for the smile is the beginning of love.",
+      },
+      {
+        imageURl:
+          "http://themes.sindevo.com/marriage-full/wp-content/uploads/2017/11/thumb2.jpg",
+        text: "Let us always meet each other with smile, for the smile is the beginning of love.",
+      },
+      {
+        imageURl:
+          "http://themes.sindevo.com/marriage-full/wp-content/uploads/2017/11/thumb3.jpg",
+        text: "Let us always meet each other with smile, for the smile is the beginning of love.",
+      },
+      {
+        imageURl:
+          "http://themes.sindevo.com/marriage-full/wp-content/uploads/2017/11/thumb4.jpg",
+        text: "Let us always meet each other with smile, for the smile is the beginning of love.",
+      },
+    ],
+    recentItems: [
+      {
+        text1:
+          "Admin on Let us always meet each other with smile, for the smile is the beginning of love.",
+        text2:
+          "Admin on Let us always meet each other with smile, for the smile is the beginning of love.",
+        text3:
+          "Admin on Let us always meet each other with smile, for the smile is the beginning of love.",
+        text4: "A WordPress Commenter on Hello world!",
+      },
+    ],
   };
 
   res.status(200).json(data);
